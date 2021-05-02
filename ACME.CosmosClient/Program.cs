8bda771f234ddb368036b099c50c4b17be0a267b
Patrick <pschmidt@pasit.nl>
2021-05-02 13:39:37 +0200

    Removed data

---
+++ ACME.CosmosClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using ACME.Documents;
+using Microsoft.Azure.Cosmos;
+
+
+namespace ACME.CosmosClientX
+{
+    class Program
+    {
+        private static string endpoint = "";
+        private static string key = "";
+
+        static void Main(string[] args)
+        {
+            ReadCustomers();
+            Console.WriteLine("Done!");
+        }
+
+        private static void ReadCustomers()
+        {
+            var client = new CosmosClient(endpoint, key);
+            var custOrders = client.GetDatabase("ACME").GetContainer("CustomerOrders");
+            var query = custOrders.GetItemLinqQueryable<Customer>(allowSynchronousQueryExecution:true).Take(100);
+
+            foreach(var customer in query)
+            {
+                Console.WriteLine($"[{customer.Id}] {customer.Firstname} {customer.Lastname}");
+            }
+
+
+        }
+    }
+}
